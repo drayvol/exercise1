0b417acd5ffeb857ca70478a75d77c60066b7175
aleksandr <drayvol@gmail.com>
2022-09-22 23:18:26 +0300

    Сдано дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
+        // Задача  1
         var dog = 8;
         var cat = 3.6;
         var paper =763789;
