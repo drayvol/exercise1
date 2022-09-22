4bb8282b827994e29d150a7f9a8f7a198df7f976
aleksandr <drayvol@gmail.com>
2022-09-22 23:15:43 +0300

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        var dog = 8;
+        var cat = 3.6;
+        var paper =763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        // Задача 2
+        dog+=4;
+        cat+=4;
+        paper+=4;
+        // Задача 3
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog-=3.5;
+        cat-=1.6;
+        paper-=7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
